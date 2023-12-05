b2bbc01a9fcffe6c34bf2c48833050b6e7f9acd9
alexandr_mamontov <sasham13101999@yandex.ru>
2023-12-06 01:35:14 +0300

    ready main

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-new Swiper('.swiper-container', {
+let mainSlider = new Swiper('.board__slider', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -18,9 +18,7 @@ new Swiper('.swiper-container', {
   slidesPerView: 1,
 
   // autoplay: {
-  //   // Пауза между прокруткой
   //   delay: 1000,
-  //   // Закончить на последнем слайде
   //   stopOnLastSlide: false,
   // },
 
@@ -28,47 +26,17 @@ new Swiper('.swiper-container', {
   spaceBetween: 30,
   direction: 'vertical',
   effect: 'slide',
-})
 
-
-let tabsBtn = document.querySelectorAll('.board__btn');
-let tabsStages = document.querySelectorAll('.board__slider');
-
-tabsBtn.forEach(function(element) {
-  element.addEventListener('click', function(e) {
-    const path = e.currentTarget.dataset.path;
-
-    tabsBtn.forEach(function(btn) {
-      btn.classList.remove('board__btn-post--active')
-    });
-
-    e.currentTarget.classList.add('board__btn-post--active');
-
-    tabsStages.forEach(function(element) {
-      element.classList.remove('board__slider--active')
-    });
-
-    document.querySelector(`[data-target="${path}"]`).classList.add('board__slider--active');
-  });
-});
-
-document.querySelector(`[data-path="one"]`).addEventListener('click', function() {
-  mast.classList.remove('board__btn--active')
-  spec.classList.add('board__btn--active')
-})
-
-document.querySelector(`[data-path="two"]`).addEventListener('click', function() {
-  spec.classList.remove('board__btn--active')
-  mast.classList.add('board__btn--active')
-})
-
-let spec = document.querySelector('.board-specialist');
-let mast = document.querySelector('.board-master');
-spec.addEventListener('click', function() {
-  spec.classList.remove('board__btn--active')
-  mast.classList.add('board__btn--active')
-})
-mast.addEventListener('click', function() {
-  mast.classList.remove('board__btn--active')
-  spec.classList.add('board__btn--active')
+  on: {
+    slideChange: function () {
+      let activeSlide = this.slides[this.activeIndex];
+      let newText = activeSlide.querySelector('.board__cite').innerHTML;
+      document.querySelector('.board__text').style.opacity = '0';
+      document.querySelector('.board__text').style.transition = 'opacity .3s ease-in-out';
+      setTimeout(() => {
+        document.querySelector('.board__text').innerHTML = newText;
+        document.querySelector('.board__text').style.opacity = '1';
+      }, 500)
+    }
+  }
 })
