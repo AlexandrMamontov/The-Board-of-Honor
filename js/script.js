e6ba2ab16d17a26256374be7f221c0385956b893
alexandr_mamontov <sasham13101999@yandex.ru>
2023-12-18 13:17:16 +0300

    ready main

--- js/script.js
+++ js/script.js
@@ -27,18 +27,19 @@ let mainSlider = new Swiper('.board__slider', {
   direction: 'vertical',
   effect: 'slide',
 
-  on: {
-    slideChange: function () {
-      let activeSlide = this.slides[this.activeIndex];
-      let newText = activeSlide.querySelector('.board__cite').innerHTML;
-      document.querySelector('.board__text').style.opacity = '0';
-      document.querySelector('.board__text').style.transition = 'opacity .3s ease-in-out';
-      setTimeout(() => {
-        document.querySelector('.board__text').innerHTML = newText;
-        document.querySelector('.board__text').style.opacity = '1';
-      }, 500)
-    }
-  },
+  // Alex убрал данную настройку
+  // on: {
+  //   slideChange: function () {
+  //     let activeSlide = this.slides[this.activeIndex];
+  //     let newText = activeSlide.querySelector('.board__cite').innerHTML;
+  //     document.querySelector('.board__text').style.opacity = '0';
+  //     document.querySelector('.board__text').style.transition = 'opacity .3s ease-in-out';
+  //     setTimeout(() => {
+  //       document.querySelector('.board__text').innerHTML = newText;
+  //       document.querySelector('.board__text').style.opacity = '1';
+  //     }, 500)
+  //   }
+  // },
 
   breakpoints: {
     768: {
@@ -47,5 +48,30 @@ let mainSlider = new Swiper('.board__slider', {
     320: {
       direction: 'horizontal',
     },
+  },
+})
+
+// Alex добавил настройки слайдера, в котором меняется текст
+let textSlider = new Swiper('.board__cite', {
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+
+  slidesPerView: 1,
+
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  speed: 1000,
+  direction: 'horizontal',
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
   },
 })
